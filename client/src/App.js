403cf7d74464f231d1805f41ca80a3cb1eae4647
panduprasetyap <prasetya038@gmail.com>
2022-03-02 14:39:09 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import Profile from './PortofolioContainer/Home/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
